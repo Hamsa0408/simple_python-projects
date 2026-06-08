0f0819ea015683e5ef141d376d69f25fb3c2684c
Hamsa0408 <pinghamsa@gmail.com>
2026-06-08 14:45:17 +0530

    Add files via upload

---
+++ clock_timer.py
@@ -0,0 +1,36 @@
+import time
+import sys
+
+def userChoice(choice):
+    if choice == "1":
+        digital_clock()
+    elif choice == "2":
+        seconds = int(input("Enter the number of seconds to countdown: "))
+        countdown_timer(seconds)
+    else:
+        print("Invalid choice!")
+
+def digital_clock():
+    while True:
+        current_time = time.strftime("%H:%M:%S", time.localtime())
+        print("\rDigital Clock: " + current_time, end = '')
+        time.sleep(1)
+    
+def countdown_timer(seconds):
+    print("Countdown Timer started!")
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        print(f"{mins:02d}:{secs:02d}", end="\r")
+        time.sleep(1)
+        seconds -= 1
+
+    print("\nTime's up!")
+
+
+
+
+
+if __name__ == '__main__':
+    while True:
+        choice = input("Choose an option (1:Digital Clock, 2:Countdown Timer): ")
+        userChoice(choice)
